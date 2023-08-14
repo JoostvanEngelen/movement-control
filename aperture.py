4456c2e83436456e66790885ea5314417af05710
JoostvanEngelen <105918316+JoostvanEngelen@users.noreply.github.com>
2023-08-14 17:01:46 +0200

    new apertures

--- aperture.py
+++ aperture.py
@@ -38,6 +38,41 @@ class EllipcicalAperture:
         return safe_seq
 
 
+class RectangularAperture:
+    def __init__(self, width, height, center: tuple, angle=0):
+        self.aperture_shape = 'rectangular'
+        self.width = width
+        self.height = height
+        self.center_x = center[0]
+        self.center_y = center[1]
+        self.center = center
+        self.angle = angle
+        
+        self.cos_angle = np.cos(np.radians(180. - angle))
+        self.sin_angle = np.sin(np.radians(180. - angle))
+
+    def point_inside_aperture(self, point):
+        xc = point[0] - self.center[0]
+        yc = point[1] - self.center[1]
+
+        xct = xc * self.cos_angle - yc * self.sin_angle
+        yct = xc * self.sin_angle + yc * self.cos_angle 
+
+        half_width = self.width / 2
+        half_height = self.height / 2
+
+        return abs(xct) <= half_width and abs(yct) <= half_height
+
+    def remove_points_outside_aperture(self, seq):
+        safe_seq = []
+        removed_points = []
+        for point in seq:
+            if self.point_inside_aperture(point):
+                safe_seq.append(point)
+            else:
+                removed_points.append(point)
+        return safe_seq
+  
 
 def create_raster_sequence(center, size, resolution):
     # This is now just a square centered around the middle but could be anything
@@ -56,29 +91,22 @@ def create_raster_sequence(center, size, resolution):
     return sequence
 
 
-# def measurement_sequence(LTS_system, sequence):
-#     for x_co, y_co in sequence:
-#         LTS_system.move_to(x_co, y_co)
-#         # HERE DO THE MEASUREMENT
-#         time.sleep(0.5) # this is a replacement for some measurement
-#     return
+el = EllipcicalAperture(50, 150, (150,150), angle=0)
+# el = RectangularAperture(8, 4, (.5, 1), angle=40)
 
-
-
-
-el = EllipcicalAperture(5, 10, (0,0))
-
-seq = create_raster_sequence((0,0), 10, 20)
-# plt.scatter(*zip(*seq))
-# plt.xlim([-5, 5])
-# plt.ylim([-5, 5])
-# plt.show()
+seq = create_raster_sequence((150,150), 300, 40)
+plt.scatter(*zip(*seq))
+plt.xlim([-5, 305])
+plt.ylim([-5, 305])
+plt.grid()
+plt.show()
 
 
 new_seq = el.remove_points_outside_aperture(seq)
 plt.plot(*zip(*new_seq), '-bo')
-plt.xlim([-5, 5])
-plt.ylim([-5, 5])
+plt.xlim([-5, 305])
+plt.ylim([-5, 305])
+plt.grid()
 plt.show()
 
 
